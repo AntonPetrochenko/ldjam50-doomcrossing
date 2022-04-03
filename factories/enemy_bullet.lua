919d65b8e3b3f87023463a1c7856c4ca7695ff28
IHateBeingAlone <aroww200@gmail.com>
2022-04-03 17:47:07 +0600

    132123123

---
+++ factories/enemy_bullet.lua
@@ -0,0 +1,57 @@
+local gradient = require 'gradient_mesh'
+
+local bullet_gradient = gradient('horizontal', {0.5,0,0,0},{0.74,0.76,0.13,1}, {1,0,0,1}, {0.5,0.35,0.75,1}, {0,0,1,1})
+
+return function (x,y,vel, damping, angle, cut)
+  local new_bullet = {}
+
+  new_bullet.x = x
+  new_bullet.y = y
+
+  new_bullet.damping = damping -- 1
+
+  new_bullet.angle = angle -- kakoy nravitsya
+
+  new_bullet.vel = vel --p do p
+
+  new_bullet.is_bullet = true
+
+  new_bullet.collides = true
+  new_bullet.pw = 8
+  new_bullet.ph = 8
+  new_bullet.on_collision = function (self, other)
+    if other.isplayer then
+      return
+    else
+      return --world:del(self)
+    end
+  end
+
+  new_bullet.life = cut
+
+  function new_bullet.update(self, dt)
+
+    self.vel = self.vel * self.damping
+
+    self.life = self.life - dt
+
+    self.x = self.x + math.cos(angle)*self.vel
+    self.y = self.y + math.sin(angle)*self.vel
+
+    self:finalize_motion()
+
+    if self.life < 0 then
+      self.damping = 0.9
+    end
+    
+    if self.vel < 0.1 then world:del(self) end
+  end
+
+  function new_bullet.draw(self)
+
+    local length = 8 * (  math.log( math.pow(self.vel, 0.6)  ) + 1.5 )
+
+    love.graphics.draw(bullet_gradient, self.x+4, self.y+4, self.angle, length, 8, 0.5, 0.5 )
+  end
+  return new_bullet
+end
--- factories/shooting_enemy.lua
+++ factories/shooting_enemy.lua
@@ -1,9 +1,30 @@
 local base_enemy = require('factories.base_enemy')
+local enemy_bullet = require 'factories.enemy_bullet'
 
 return function (...)
   local new_enemy = base_enemy(...)
 
   new_enemy.class = 'random_user_xy'
+
+  new_enemy.shoot_x = 24/2
+  new_enemy.shoot_y = 32/2
+  new_enemy.weapon = {
+    impulse = 2,
+    damping = 1,
+    cut = 0.1,
+
+    spread = 1,
+    spray = 0.1,
+
+    number = 1,
+    damage = 1,
+
+    rate = 0.3,
+    rumble_strength = 0.5,
+    rumble_duration = 0.1,
+
+    spread_amount = 1.2
+}
 
   function new_enemy.extra_update(self, dt)
     self:walk_movement(dt)
@@ -15,6 +36,13 @@ return function (...)
     if math.random(0, 100) > 80 then
       self.delta_x = 0
       self.delta_y = 0
+      local spread_velocity = self.weapon.impulse - (math.random()-0.5)*self.weapon.spread
+      world:add(enemy_bullet(self.x+self.shoot_x, self.y+self.shoot_y, spread_velocity, self.weapon.damping, 1, self.weapon.cut))
+      world:add(enemy_bullet(self.x+self.shoot_x, self.y+self.shoot_y, spread_velocity, self.weapon.damping, 2, self.weapon.cut))
+      world:add(enemy_bullet(self.x+self.shoot_x, self.y+self.shoot_y, spread_velocity, self.weapon.damping, 4, self.weapon.cut))
+      world:add(enemy_bullet(self.x+self.shoot_x, self.y+self.shoot_y, spread_velocity, self.weapon.damping, 3, self.weapon.cut))
+      world:add(enemy_bullet(self.x+self.shoot_x, self.y+self.shoot_y, spread_velocity, self.weapon.damping, 5, self.weapon.cut))
+      world:add(enemy_bullet(self.x+self.shoot_x, self.y+self.shoot_y, spread_velocity, self.weapon.damping, 6, self.weapon.cut))
     else
       self.speed = 0.5
       local nearest = self:nearestPlayer()
